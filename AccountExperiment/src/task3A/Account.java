c49811434d46e214a8b37e0c3523edb6c36531f9
jlibera94 <jlibera94@hotmail.com>
2017-10-12 18:06:24 -0400

    Update Account.java

--- AccountExperiment/src/task3A/Account.java
+++ AccountExperiment/src/task3A/Account.java
@@ -140,7 +140,7 @@ public class Account {
 	 * A method that allows a customer to deposit money into this account
 	 * @param amount A double that represents a deposit amount
 	 */
-	public synchronized void debosit(double amount){
+	public synchronized void debosit(double amount){ //Method Synchronization
 		
 		// Waste some time doing fake computations
 		// do not remove or modify any of the following 3 statements
@@ -162,7 +162,7 @@ public class Account {
 	 * A method that allows a customer to withdraw money from this account
 	 * @param amount A double that represents a withdrawal amount
 	 */
-	public synchronized void withdraw(double amount){
+	public synchronized void withdraw(double amount){  ///Method Synchronization
 
 		// Waste some time doing fake computations
 		// do not remove or modify any of the following 3 statements
